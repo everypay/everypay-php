9730c18af0f75bc00b724c510c17c1d80bad1f13
akDeveloper <andreas.kollaros@gmail.com>
2015-10-07 16:10:04 +0300

    remove outdated examples.

--- examples/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once '../vendor/autoload.php';
-
-/**
- * Don't forget this!
- */
-$key = 'sk_SJVEJD6ciomRUYu7zVpkKjM6gPgbFIKs';
-Everypay\Everypay::setApiKey($key);
--- examples/example1.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Example 1
- * 
- * Create a card token and proceed to a payment.
- * 
- */
-
-require_once 'bootstrap.php';
