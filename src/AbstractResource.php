b79f865492549c5f0dfdd33dc5ab6c6087b57b60
rigas <nrigas@everypay.gr>
2021-04-13 14:59:48 +0300

    Forced ssl version removed.

--- src/AbstractResource.php
+++ src/AbstractResource.php
@@ -159,7 +159,6 @@ abstract class AbstractResource
         $client->setOption(CurlClient::TIMEOUT, 30);
         $client->setOption(CurlClient::SSL_VERIFY_PEER, 0);
         $client->setOption(CurlClient::USER_AGENT, 'EveryPay PHP Library ' . Everypay::VERSION);
-        $client->setOption(CurlClient::SSL_VERSION, CURL_SSLVERSION_TLSv1_2);
 
         return $client;
     }
